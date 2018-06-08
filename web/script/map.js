14b7e2788eece2d91f8d63733a885bb88a150725
Unknown <denys.pasiuha@nure.ua>
2018-06-08 21:21:41 +0300

    Edit page "Map"

---
+++ web/script/map.js
@@ -0,0 +1,13 @@
+function map() {
+	var mapOptions = {
+		center: new google.maps.LatLng(50.01502774, 36.22808368),
+		zoom: 18,
+		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		disableDefaultUI: true,
+		clickableIcons: false,
+		disableDoubleClickZoom: true,
+		draggable: false,
+		draggableCursor: "default"
+	}
+	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
